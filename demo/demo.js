d60be7b2f6f71ba87a1b254beb3402dd96e83a98
Jason <jasonlikenfs@gmail.com>
2016-08-01 12:54:12 +0800

    add demo

---
+++ demo/demo.js
@@ -0,0 +1,57 @@
+
+angular.module('demo', ['lyTable', 'lyPagination'])
+    .controller('Demo', function($scope) {
+      $scope.columns = [
+        {
+          field: 'name',
+          name: 'Username',
+          formatter: simpleFormatter
+        },
+        {
+          field: 'age',
+          name: 'Age',
+          formatter: simpleFormatter
+        },
+        {
+          field: 'address',
+          name: 'Address',
+          autoEscape: true,
+          formatter: simpleFormatter,
+          cssClass: 'text-center',
+          headCssClass: 'text-center',
+          testClick: function(rowData, rowindex, columnindex) {
+            console.log(arguments);
+          }
+        }
+      ];
+      var temps = [];
+      for(var i = 1; i< 88; i++) {
+        var temp = {
+          name: 'jason',
+          age: i,
+          address: '<p>shanghai china 2</p>'
+        };
+        temps.push(temp);
+      }
+      $scope.data = temps.slice(0, 10);
+
+      $scope.totalRecords = temps.length;
+      $scope.pageSize = 10;
+      $scope.currentPage = 1;
+      $scope.autoReset = true;
+      $scope.pageDisplayNumber = 10;
+      $scope.$broadcast('resetPagination', $scope.totalRecords);
+      $scope.$on('pageChange', function (e, currentPage) {
+        $scope.currentPage = currentPage;
+        var startIndex = (currentPage - 1) * $scope.pageSize;
+        var endIndex = startIndex + $scope.pageSize;
+        $scope.data = temps.slice(startIndex, endIndex);
+      });
+      $scope.postInit = function(ele) {
+        console.log('postInit');
+      };
+      function simpleFormatter(fieldData, rowData, columnDefinition) {
+        return fieldData;
+      }
+    });
+
